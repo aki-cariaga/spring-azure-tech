53185a67ce4d64ff7afac815d3cd7416e9458991
akicariaga <akicariaga@yahoo.com>
2024-07-07 14:41:45 +0800

    Update SpringAzureTechApplication.java
    update main class

--- src/main/java/com/easywebtech/azure/SpringAzureTechApplication.java
+++ src/main/java/com/easywebtech/azure/SpringAzureTechApplication.java
@@ -2,12 +2,20 @@ package com.easywebtech.azure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringAzureTechApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAzureTechApplication.class, args);
+	}
+	
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats you deployed the web application!";
 	}
 
 }
