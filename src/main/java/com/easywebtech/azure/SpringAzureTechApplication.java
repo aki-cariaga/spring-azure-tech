df6aabdbb5488238ddbfd6624015b49ffc26f7f2
akicariaga <akicariaga@yahoo.com>
2024-07-07 14:00:39 +0800

    first commit

---
+++ src/main/java/com/easywebtech/azure/SpringAzureTechApplication.java
@@ -0,0 +1,13 @@
+package com.easywebtech.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easywebtech/spring_azure_tech/SpringAzureTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easywebtech.spring_azure_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
